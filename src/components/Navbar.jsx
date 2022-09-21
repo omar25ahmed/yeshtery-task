2397c4924a96c9d3faed1acf389ac8571b60c74b
Omar Ragheb <ahmed.raph29@gmail.com>
2022-09-21 20:57:18 +0300

    finsh first part of the nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,32 +5,36 @@ import React from 'react';
 
 const navbar = () => (
   <nav>
-    <div className="first-nav container d-flex">
-      <div className="logo">
-        <img src="images/hamburger.png" alt="hamburger" className="nav-img" />
-        <img src="/images/yeshtery-logo.png" alt="logo" className="nav-img" />
-      </div>
-      <div>
-        <p>
-          &lt; Valentine&nps;s Day Offers! Buy Two Get One Free
-          {' '}
-          <span className="font-weight-bold text-decoration-underline">Shop Now</span>
-          {' '}
-          &gt;
-        </p>
-      </div>
-      <div className="d-flex">
-        <div>
-          <img src="/images/contact.png" alt="phone" className="nav-img" />
-          <span>Contact Us</span>
+    <div className="first-nav">
+      <div className="container d-flex justify-content-around w-75 pt-2 align-items-center">
+        <div className="logo">
+          <img src="images/hamburger.png" alt="hamburger" className="nav-img" />
+          <img src="/images/yeshtery-logo.png" alt="logo" className="nav-img" />
         </div>
         <div>
-          <img src="/images/track.png" alt="phone" className="nav-img" />
-          <span>Track Order</span>
+          <p className="m-0">
+            <span className="symbol">&lt;</span>
+            {' '}
+            Valentine&apos;s Day Offers! Buy Two Get One Free
+            {' '}
+            <span className="fw-bold text-decoration-underline" role="button">Shop Now</span>
+            {' '}
+            <span className="symbol">&gt;</span>
+          </p>
         </div>
-        <div>
-          <img src="/images/find.png" alt="phone" className="nav-img" />
-          <span>Find A Store</span>
+        <div className="d-flex gap-4">
+          <div>
+            <img src="/images/contact.png" alt="phone" className="nav-img" />
+            <span className="fw-bold" role="button">Contact Us</span>
+          </div>
+          <div>
+            <img src="/images/track.png" alt="phone" className="nav-img" />
+            <span className="fw-bold" role="button">Track Order</span>
+          </div>
+          <div>
+            <img src="/images/find.png" alt="phone" className="nav-img" />
+            <span className="fw-bold" role="button">Find A Store</span>
+          </div>
         </div>
       </div>
     </div>
