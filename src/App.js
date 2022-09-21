59e1651e0867730f9ac53bd59c2bd4c9de49fe10
Omar Ragheb <ahmed.raph29@gmail.com>
2022-09-21 23:41:55 +0300

    Add all parts of the navbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
+import React, { Component } from 'react';
 import Navbar from './components/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <div className="dummy">
-        lorem ipsum dolor sit amet, consectetur adipiscing elit,
-        sed do eiusmod tempor incididunt ut labore et dolore
-        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat.
-      </div>
-    </div>
-  );
-}
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-export default App;
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="dummy">
+          lorem ipsum dolor sit amet, consectetur adipiscing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+          laboris nisi ut aliquip ex ea commodo consequat.
+        </div>
+      </div>
+    );
+  }
+}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,43 +1,124 @@
 import './navbar.scss';
-import React from 'react';
-// import hamurger from '../../public/images/hamburger.png';
-// import logo from '../../public/images/logo.png';
+import React, { Component } from 'react';
 
-const navbar = () => (
-  <nav>
-    <div className="first-nav">
-      <div className="container d-flex justify-content-around w-75 pt-2 align-items-center">
-        <div className="logo">
-          <img src="images/hamburger.png" alt="hamburger" className="nav-img" />
-          <img src="/images/yeshtery-logo.png" alt="logo" className="nav-img" />
+export default class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <nav>
+        <div className="first-nav">
+          <div className="container d-flex justify-content-between w-75 pt-2 align-items-center">
+            <div className="logo">
+              <img src="images/hamburger.png" alt="hamburger" className="nav-img" />
+              <img src="/images/yeshtery-logo.png" alt="logo" className="nav-img" />
+            </div>
+            <div>
+              <p className="m-0">
+                <span className="symbol">&lt;</span>
+                {' '}
+                Valentine&apos;s Day Offers! Buy Two Get One Free
+                {' '}
+                <span className="fw-bold text-decoration-underline" role="button">
+                  Shop Now
+                </span>
+                {' '}
+                <span className="symbol">&gt;</span>
+              </p>
+            </div>
+            <div className="d-flex gap-4">
+              <div>
+                <img src="/images/contact.png" alt="phone" className="nav-img" />
+                <span className="fw-bold" role="button">
+                  Contact Us
+                </span>
+              </div>
+              <div>
+                <img src="/images/track.png" alt="phone" className="nav-img" />
+                <span className="fw-bold" role="button">
+                  Track Order
+                </span>
+              </div>
+              <div>
+                <img src="/images/find.png" alt="phone" className="nav-img" />
+                <span className="fw-bold" role="button">
+                  Find A Store
+                </span>
+              </div>
+            </div>
+          </div>
         </div>
-        <div>
-          <p className="m-0">
-            <span className="symbol">&lt;</span>
-            {' '}
-            Valentine&apos;s Day Offers! Buy Two Get One Free
-            {' '}
-            <span className="fw-bold text-decoration-underline" role="button">Shop Now</span>
-            {' '}
-            <span className="symbol">&gt;</span>
-          </p>
-        </div>
-        <div className="d-flex gap-4">
+        <div className="second-nav container d-flex justify-content-between align-items-center">
           <div>
-            <img src="/images/contact.png" alt="phone" className="nav-img" />
-            <span className="fw-bold" role="button">Contact Us</span>
+            <input type="text" placeholder="Search" className="search" />
           </div>
           <div>
-            <img src="/images/track.png" alt="phone" className="nav-img" />
-            <span className="fw-bold" role="button">Track Order</span>
+            <img src="/images/Group 745.svg" alt="cart" className="brand-img" />
           </div>
-          <div>
-            <img src="/images/find.png" alt="phone" className="nav-img" />
-            <span className="fw-bold" role="button">Find A Store</span>
+          <div className="d-flex gap-4 align-items-center">
+            <div className="d-flex gap-2">
+              <p className="cart">0</p>
+              <img src="/images/Path 772.svg" alt="user" className="nav-img" />
+              <span className="fw-bold" role="button">
+                Cart
+              </span>
+            </div>
+            <div className="d-flex gap-2">
+              <img src="/images/Path 771.svg" alt="user" className="nav-img" />
+              <span className="fw-bold" role="button">
+                Wishlist
+              </span>
+            </div>
+            <div className="d-flex gap-2">
+              <img src="/images/Path 773.svg" alt="user" className="nav-img" />
+              <span className="fw-bold" role="button">
+                Login
+              </span>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
-  </nav>
-);
-export default navbar;
+        <div className="third-nav bg-dark">
+          <div className="container d-flex justify-content-between align-items-center">
+            <button type="button" className="text-light border-0 bg-transparent fw-bold">
+              Men
+            </button>
+            <button type="button" className="text-light border-0 bg-transparent fw-bold">
+              Women
+            </button>
+            <button type="button" className="text-light border-0 bg-transparent fw-bold">
+              Unisex
+            </button>
+            <button type="button" className="text-light border-0 bg-transparent fw-bold">
+              Kids
+            </button>
+            <button type="button" className="text-light border-0 bg-transparent fw-bold">
+              Best Sellers
+            </button>
+            <button type="button" className="text-light border-0 bg-transparent fw-bold">
+              New Arrivals
+            </button>
+            <button type="button" className="text-danger border-0 bg-transparent fw-bold">
+              Offers
+            </button>
+          </div>
+        </div>
+        <div className="fourth-nav">
+          <div className="container d-flex justify-content-start align-items-center">
+            <span className="text-decoration-underline me-2" role="button">Men</span>
+            /
+            <span className="text-decoration-underline mx-2" role="button">Clothing</span>
+            /
+            <span className="text-decoration-underline mx-2" role="button">Tops</span>
+            /
+            <span className="text-decoration-underline mx-2" role="button">Addidas</span>
+            /
+            Addidas Black T-Shirt
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
