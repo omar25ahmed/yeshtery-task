5873a0c1d19e616e3494db876efc0a80b5f749e0
Omar Ragheb <ahmed.raph29@gmail.com>
2022-09-22 20:38:34 +0300

    Create product description component

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,26 +1,25 @@
 import React, { Component } from 'react';
+import ProductDetails from '../components/ProductDetails';
+import { http } from '../services/http';
+import './Product.scss';
 
 export default class Product extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      data: [],
+    };
+  }
+
+  async componentDidMount() {
+    this.setState({ data: await http() });
   }
 
   render() {
+    // const { productId } = this.state;
+    const { data } = this.state;
     return (
-      <div className="d-flex justify-content-between">
-        <div className="">
-          <h1>Product image</h1>
-        </div>
-        <div className="">
-          <img src="/images/Path 771.svg" alt="Adidas" />
-          <h2>Product Name</h2>
-          <p>Category</p>
-          <div>
-            <p>Price</p>
-          </div>
-        </div>
-      </div>
+      <ProductDetails data={data} />
     );
   }
 }
