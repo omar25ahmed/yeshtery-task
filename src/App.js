d954f652e6e367284df0033bbfd9bed6193bb550
Omar Ragheb <ahmed.raph29@gmail.com>
2022-09-23 20:52:57 +0300

    final touches

--- src/App.js
+++ src/App.js
@@ -2,10 +2,23 @@ import React, { Component } from 'react';
 import {
   BrowserRouter as Router, Routes, Route, Navigate,
 } from 'react-router-dom';
+import ClipLoader from 'react-spinners/ClipLoader';
 import Navbar from './components/Navbar';
-import Product from './pages/Product';
 import Footer from './components/Footer';
 import { ProductProvider } from './services/context';
+
+const Product = React.lazy(() => import('./pages/Product'));
+
+const override = {
+  position: 'absolute',
+  top: '0',
+  bottom: '0',
+  right: '0',
+  left: '0',
+  margin: 'auto auto',
+  height: '100px',
+  width: '100px',
+};
 
 export default class App extends Component {
   constructor(props) {
@@ -15,16 +28,18 @@ export default class App extends Component {
 
   render() {
     return (
-      <ProductProvider>
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route path="*" element={<Navigate to="/product/1" />} />
-            <Route path="/product/:id" element={<Product />} />
-          </Routes>
-          <Footer />
-        </Router>
-      </ProductProvider>
+      <React.Suspense fallback={<ClipLoader cssOverride={override} />}>
+        <ProductProvider>
+          <Router>
+            <Navbar />
+            <Routes>
+              <Route path="*" element={<Navigate to="/product/1" />} />
+              <Route path="/product/:id" element={<Product />} />
+            </Routes>
+            <Footer />
+          </Router>
+        </ProductProvider>
+      </React.Suspense>
     );
   }
 }
